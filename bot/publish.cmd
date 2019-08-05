ac3499c0ec35ac3bcf82de34dc7bb018de815138
ChrisProlls <christophe.gigax@versusmind.eu>
2019-08-05 13:19:13 +0200

    Bot + Extension

---
+++ bot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=vscodebattlebot-Web-Deploy.pubxml -p:Password=ug7804wourd1yp8l1CR0icbsF4st7FXBXxsNeGGYphQfgpFKevWszLcjfet4
+
---
+++ extension/src/extension.ts
@@ -0,0 +1,48 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "code-review-assistant" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		const panel = vscode.window.createWebviewPanel(
+			'codeReviewAssistant',
+			'Code Review Assistant',
+			vscode.ViewColumn.One,
+			{}
+		);
+
+		panel.webview.html = getWebviewContent();
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
+
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Code Review</title>
+  </head>
+  <body>
+	  <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+  </body>
+  </html>`;
+}
