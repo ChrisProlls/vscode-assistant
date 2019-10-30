491639c112b0622c950c03ceba3c0007124f241c
Christophe GIGAX <christophe.gigax@versusmind.eu>
2019-10-30 16:50:16 +0100

    More matrix !!

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -3,6 +3,8 @@
 import * as vscode from 'vscode';
 import * as ws from "ws";
 import * as xhr2 from "xhr2";
+import * as fs from 'fs';
+import * as path from 'path';
 import { DirectLine } from 'botframework-directlinejs';
 
 class Message {
@@ -55,21 +57,20 @@ export async function activate(context: vscode.ExtensionContext) {
 			webSocket: false
 		});
 
-		context.subscriptions.push(vscode.commands.registerCommand('extension.codeReview.dialog', () => showInputBox(directLine, panel)));
+		context.subscriptions.push(vscode.commands.registerCommand('extension.codeReview.dialog', () => showInputBox(directLine, panel, context.extensionPath)));
 
 		directLine.activity$
 			.subscribe(
 				(activity: any) => {
 					if (activity.text && activity.from.id !== user) {
-						messages.push({ content: activity.text, bot: true, response: activity.channelData });
-						refreshView(panel);
+						sendMessage(panel, { content: activity.text, bot: true, response: activity.channelData });
 					}
 
 					console.log("received activity ", activity);
 				}
 			);
 
-		refreshView(panel);
+		panel.webview.html = getWebviewContent(context.extensionPath);	
 	});
 
 	context.subscriptions.push(disposable);
@@ -92,7 +93,7 @@ function getErrors() {
 	return errors;
 }
 
-function showInputBox(directLine: DirectLine, panel: vscode.WebviewPanel) {
+function showInputBox(directLine: DirectLine, panel: vscode.WebviewPanel, extensionPath: string) {
 	vscode.window
 		.showInputBox()
 		.then(text => {
@@ -100,8 +101,7 @@ function showInputBox(directLine: DirectLine, panel: vscode.WebviewPanel) {
 				return;
 			}
 
-			messages.push({ content: text, bot: false });
-			refreshView(panel);
+			sendMessage(panel, { content: text, bot: false });
 
 			const errors = getErrors();
 
@@ -119,130 +119,19 @@ function showInputBox(directLine: DirectLine, panel: vscode.WebviewPanel) {
 		});
 }
 
-function refreshView(panel: vscode.WebviewPanel) {
-	panel.webview.html = getWebviewContent();
+function sendMessage(panel: vscode.WebviewPanel, message: Message) {
+	panel.webview.postMessage(message);
 }
 // this method is called when your extension is deactivated
 export function deactivate() { }
 
-function getWebviewContent() {
-	return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title>Code Review</title>
-	  <style>
-		.message-content {
-			padding: 3px 7px;
-			background-color: SteelBlue;
-			border-radius: 5px;
-			display: inline-block;
+function getWebviewContent(extensionPath: string) {
+	const content = fs.readFileSync(
+		path.join(extensionPath, 'assets', 'index.html'),
+		{
+			encoding: 'utf8'
 		}
+	);
 
-		.message-container {
-			margin: 20px;
-		}
-
-		.message-bot {
-			background-color: SeaGreen;
-		}
-
-		canvas { display: block; }
-
-		html, body {
-			height: 800px;
-			width: 100%
-		}
-
-		* {margin: 0; padding: 0;}
-	  </style>
-  </head>
-  <body>
-  	  <canvas id="c"></canvas>
-	  <!--${getMessages()}-->
-
-	  <script> 
-		function launchMatrix() {
-			var c = document.getElementById("c");
-			var ctx = c.getContext("2d");
-
-			//making the canvas full screen
-			c.height = window.innerHeight;
-			c.width = window.innerWidth;
-
-			console.log();
-
-			//chinese characters - taken from the unicode charset
-			var chinese = "田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑";
-			//converting the string into an array of single characters
-			chinese = chinese.split("");
-
-			var font_size = 10;
-			var columns = c.width/font_size; //number of columns for the rain
-			//an array of drops - one per column
-			var drops = [];
-			//x below is the x coordinate
-			//1 = y co-ordinate of the drop(same for every drop initially)
-			for(var x = 0; x < columns; x++)
-				drops[x] = 1; 
-
-			//drawing the characters
-			function draw()
-			{
-				//Black BG for the canvas
-				//translucent BG to show trail
-				ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-				ctx.fillRect(0, 0, c.width, c.height);
-				
-				ctx.fillStyle = "#0F0"; //green text
-				ctx.font = font_size + "px arial";
-				//looping over drops
-				for(var i = 0; i < drops.length; i++)
-				{
-					//a random chinese character to print
-					var text = chinese[Math.floor(Math.random()*chinese.length)];
-					//x = i*font_size, y = value of drops[i]*font_size
-					ctx.fillText(text, i*font_size, drops[i]*font_size);
-					
-					//sending the drop back to the top randomly after it has crossed the screen
-					//adding a randomness to the reset to make the drops scattered on the Y axis
-					if(drops[i]*font_size > c.height && Math.random() > 0.975)
-						drops[i] = 0;
-					
-					//incrementing Y coordinate
-					drops[i]++;
-				}
-			}
-
-			setInterval(draw, 33);
-		}
-
-		setTimeout(() => {
-			  //window.scrollTo(0,document.body.scrollHeight);
-
-			  	launchMatrix();
-		}, 1000);
-	  </script>
-  </body>
-  </html>`;
-}
-
-function getMessages() {
-	return messages
-		.map(message => {
-			var content = message.content;
-
-			if(message.response && message.response.IsImage) {
-					content = `<img src="${message.content}">`;
-			}
-
-			return `<div class='message-container'>
-						<div class='message-content ${(message.bot ? 'message-bot' : '')}'>
-							${content}
-						</div>
-					</div>
-					`;
-		})
-		.join('');
+	return content;
 }
